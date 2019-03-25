a51003e9e86457f66b3e5c77da540b8e64ccd1cb
imckly <dkly@naver.com>
2019-03-26 07:40:51 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Customer from './components/Customer'
+import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import './App.css';
 
 import Paper from '@material-ui/core/Paper';
@@ -28,10 +29,28 @@ const styles = theme => ({
 
 class App extends Component {
 
-  state = {
-    customers:"",
-    completed: 0
+  // state = {
+  //   customers:"",
+  //   completed: 0
+  // }
+  constructor(props){
+    super(props);
+    this.state = {
+      customers:"",
+      completed: 0
+    }
   }
+
+  stateRefresh = () => {
+    this.setState({
+      customers:"",
+      completed: 0
+    });
+    this.callApi()
+      .then(res=>this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
 
   //모든 컴포넌트가 마운트 되었을때 동작
   componentDidMount(){
@@ -57,29 +76,33 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>              
-            </TableRow>
-        </TableHead>
-        <TableBody>
-          { this.state.customers ? this.state.customers.map(c => { return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> )}) :
-            <TableRow>
-              <TableCell colSpan="6" align="center">
-                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-              </TableCell>
-            </TableRow>
-           }
-        </TableBody>
-      </Table>
-    </Paper>
+    <div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>              
+              </TableRow>
+          </TableHead>
+          <TableBody>
+            { this.state.customers ? this.state.customers.map(c => { return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> )}) :
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+            }
+          </TableBody>
+        </Table>
+      </Paper>
+      <CustomerAdd stateRefresh={this.stateRefresh}/>
+    </div>
+    
     );
   }
 }
--- server.js
+++ server.js
@@ -22,6 +22,9 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest: './upload'})
+
 // app.get('/api/hello', (req,res) => {
 //     res.send({message: 'Hello Express!'});
 // })
@@ -34,4 +37,25 @@ app.get('/api/customers', (req,res) => {
     );
 });
 
+app.use('/image',express.static('./upload'));
+app.post('/api/customers', upload.single('image'),(req,res)=>{
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?,?,?,?,?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image,name,birthday,gender,job];
+
+    console.log(name);
+    console.log(birthday);
+    console.log(gender);
+    console.log(job);
+
+    connection.query(sql, params,
+        (err,rows,fields) => {
+            res.send(rows);
+        }
+    );
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
