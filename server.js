31e4e04e20e407c68cae1137224ade6dfe80b162
imckly <dkly@naver.com>
2019-03-26 08:11:23 +0900

    Add Customer Delete Module

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const upload = multer({dest: './upload'})
 // })
 app.get('/api/customers', (req,res) => {
     connection.query(
-        "SELECT * FROM CUSTOMER",
+        "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
         (err,rows,fields)=>{
             res.send(rows);
         }
@@ -39,7 +39,7 @@ app.get('/api/customers', (req,res) => {
 
 app.use('/image',express.static('./upload'));
 app.post('/api/customers', upload.single('image'),(req,res)=>{
-    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?,?,?,?,?)';
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?,?,?,?,?,now(),0)';
     let image = '/image/' + req.file.filename;
     let name = req.body.name;
     let birthday = req.body.birthday;
@@ -58,4 +58,17 @@ app.post('/api/customers', upload.single('image'),(req,res)=>{
         }
     );
 });
+
+app.delete('/api/customers/:id',(req,res) => {
+    let sql ='UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
+    let params = [req.params.id];
+    connection.query(sql, params,
+        (err,rows,fields) => {
+            res.send(rows);
+        }
+    );
+});
+
+
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
