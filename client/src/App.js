43edb8ea646b224b30b21066392f628c0e1a17d1
imckly <dkly@naver.com>
2019-03-25 20:19:02 +0900

    Add  Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   root : {
@@ -19,17 +20,22 @@ const styles = theme => ({
   },
   table:{
     minWidth: 1080
+  },
+  progress: {
+    margin : theme.spacing.unit *2
   }
 })
 
 class App extends Component {
 
   state = {
-    customers:""
+    customers:"",
+    completed: 0
   }
 
   //모든 컴포넌트가 마운트 되었을때 동작
   componentDidMount(){
+    this.timer = setInterval(this.progress,20);
     this.callApi()
       .then(res=>this.setState({ customers: res }))
       .catch(err => console.log(err));
@@ -41,6 +47,12 @@ class App extends Component {
     const body = await response.json();
     return body;
   }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >=100 ? 0 : completed + 1});
+  }
+
 
   render() {
     const { classes } = this.props;
@@ -58,7 +70,13 @@ class App extends Component {
             </TableRow>
         </TableHead>
         <TableBody>
-          { this.state.customers ? this.state.customers.map(c => { return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> )}) : ""}
+          { this.state.customers ? this.state.customers.map(c => { return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> )}) :
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow>
+           }
         </TableBody>
       </Table>
     </Paper>
